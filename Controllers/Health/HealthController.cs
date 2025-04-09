fb823e87154c0542af556fda2b56a3daa637d56d
JONGHO-JUHN <owner@thereis.dev>
2025-04-10 02:37:01 +0900

    상태 체크용 API 컨트롤러 추가. launchSettings.json에 Local, Development, Production 실행환경 추가. 프로젝트 세팅부분에 누락된 폴더 추가.

---
+++ Controllers/Health/HealthController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GIT_KOREA_QA_API.Controllers.Health
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet("check")]
+        public IActionResult HealthCheck()
+        {
+            return Ok(new { message = "Health Check!"});
+        }
+    }
+}
